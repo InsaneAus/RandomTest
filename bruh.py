7ebbed8f86e492c0d67dd69831c93ddf50464cfc
Austin Christian Adithia <austinandadithia@gmail.com>
2022-07-14 16:30:18 +0700

    austin

--- bruh.py
+++ bruh.py
@@ -2,3 +2,4 @@ print("What does the word 'bruh' mean?")
 print("Eazy-E's real name is Eric Lynn Wright.")
 for i in range(6):
     print(i)
+print("My name is Gustavo Fring and I am a Chilean-American who works at Los Pollos Hermanos.")
